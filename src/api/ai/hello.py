897d70506ee4fd4655c5025e1d057d1ae32781ac
Arturo Parra <arturoteleco95@protonmail.ch>
2024-05-11 15:53:32 +0200

    minor improve

--- src/api/ai/hello.py
+++ src/api/ai/hello.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix="/hello")
 
 @router.get("")
 async def hello():
-    logger.info("Calling Ollama with Llama3 model")
-    result = await llm.ainvoke("Hello Ollama using Llama3!")
-    logger.info(f"Ollama response: {result}")
+    logger.info("Invoking LLM...")
+    result = await llm.ainvoke("Hello!")
+    logger.info(f"LLM result: {result}")
     return {"message": result}
