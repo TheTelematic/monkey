837d2c13d15fa612c8b1368f41e6eb2cbcd176bf
Arturo Parra <arturoteleco95@protonmail.ch>
2024-05-11 17:58:17 +0200

    move raw HTML to file until using Jinja to render it properly

--- src/api/ws.py
+++ src/api/ws.py
@@ -5,51 +5,10 @@ from core.ai import get_ai_response
 
 router = APIRouter()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>AI Chat</title>
-    </head>
-    <body>
-        <h1>AI Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button id="sendButton" >Send</button>
-            <label id="labelButton" >(Click button or Press enter to send)</label>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("/api/ws/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-                document.getElementById("sendButton").disabled = false
-                document.getElementById("labelButton").innerHTML = "(Click button or Press enter to send)"
-            };
-            function sendMessage(event) {
-                // disable button
-                document.getElementById("sendButton").disabled = true
-                // change label
-                document.getElementById("labelButton").innerHTML = "Loading..."
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
 
 @router.get("/")
 async def get():
-    return HTMLResponse(html)
+    return HTMLResponse(open("templates/chat.html").read())
 
 
 @router.websocket("/ws")
