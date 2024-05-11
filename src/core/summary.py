1159c498e8959378fa90c6b1e3ecb9cba4743eb9
Arturo Parra <arturoteleco95@protonmail.ch>
2024-05-11 17:11:24 +0200

    summary improved

--- src/core/summary.py
+++ src/core/summary.py
@@ -3,7 +3,16 @@ import asyncio
 from core.ai import get_ai_response
 
 
+def _populate_query(paragraph: str) -> str:
+    return f"Summarize the following paragraph: {paragraph}"
+
+
 async def get_summary(text: str) -> str:
     paragraphs = text.split("\n")
+    paragraphs = filter(lambda x: x.strip(), paragraphs)
+    paragraphs = map(_populate_query, paragraphs)
     results = await asyncio.gather(*[get_ai_response(paragraph) for paragraph in paragraphs])
-    return ". ".join(results)
+    new_text = ". ".join(results)
+    return await get_ai_response(
+        f"Can you remove the redundant information of the following text?\n{new_text}"
+    )
