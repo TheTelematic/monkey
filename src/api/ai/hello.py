cbef5c6c507dad7a12cc05dd3f3667af20a01a44
Arturo Parra <arturoteleco95@protonmail.ch>
2024-05-11 15:56:16 +0200

    refactor

--- src/api/ai/hello.py
+++ src/api/ai/hello.py
@@ -1,14 +1,11 @@
 from fastapi import APIRouter
 
 from core.llm import llm
-from logger import logger
 
 router = APIRouter(prefix="/hello")
 
 
 @router.get("")
 async def hello():
-    logger.info("Invoking LLM...")
-    result = await llm.ainvoke("Hello!")
-    logger.info(f"LLM result: {result}")
+    result = await llm.invoke("Hello!")
     return {"message": result}
--- src/core/llm.py
+++ src/core/llm.py
@@ -1,5 +1,18 @@
 from langchain_community.llms import Ollama
 
 import config
+from logger import logger
 
-llm = Ollama(base_url=config.LLM_URL, model="llama3")
+
+class LLM:
+    def __init__(self):
+        self.llm = Ollama(base_url=config.LLM_URL, model="llama3")
+
+    async def invoke(self, text: str) -> str:
+        logger.info("Invoking LLM...")
+        result = await self.llm.ainvoke(text)
+        logger.debug(f"LLM response: {result}")
+        return result
+
+
+llm = LLM()
