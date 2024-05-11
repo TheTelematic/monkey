15a39c432942871a94fe4f216c167729ffb7e21a
Arturo Parra <arturoteleco95@protonmail.ch>
2024-05-11 17:29:49 +0200

    improve prompt

--- src/core/summary.py
+++ src/core/summary.py
@@ -4,7 +4,10 @@ from core.ai import get_ai_response
 
 
 def _populate_query(paragraph: str) -> str:
-    return f"Summarize the following paragraph: {paragraph}"
+    return (
+        "Please summarize the following paragraph responding with just the summary and skipping any intro to it: "
+        f"{paragraph}"
+    )
 
 
 async def get_summary(text: str) -> str:
@@ -12,7 +15,4 @@ async def get_summary(text: str) -> str:
     paragraphs = filter(lambda x: x.strip(), paragraphs)
     paragraphs = map(_populate_query, paragraphs)
     results = await asyncio.gather(*[get_ai_response(paragraph) for paragraph in paragraphs])
-    new_text = ". ".join(results)
-    return await get_ai_response(
-        f"Can you remove the redundant information of the following text?\n{new_text}"
-    )
+    return " ".join(results)
