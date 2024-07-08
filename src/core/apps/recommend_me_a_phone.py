058cb70ad3028853a20bab8fedc2e5d2276552f3
TheTelematic <arturoteleco95@protonmail.ch>
2024-07-08 21:30:51 +0200

    improve prompt

--- src/core/apps/recommend_me_a_phone.py
+++ src/core/apps/recommend_me_a_phone.py
@@ -4,7 +4,7 @@ from infra.ai_wrapper import ai_engine_web_content_crawler
 from logger import logger
 
 _PROMPT = """
-Can you tell me the top 5 of best phones please? 
+Can you make me the top of best phones please? 
 Please provide name, price and some details about each phone. 
 I give you an example of the format of the output, please respect and respond using it, avoid any other comment 
 and respond with the requested information. The first character of the output must be '{' and the last one '}'.
@@ -19,5 +19,5 @@ EXAMPLE OF THE OUTPUT:
 
 async def get_phones_recommendations() -> dict:
     response = await ai_engine_web_content_crawler.invoke(_PROMPT)
-    logger.info(f"Response from Apify: {response}")
+    logger.info(f"Response from ai_engine_web_content_crawler: {response}")
     return json.loads(response)
